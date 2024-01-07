9899746de20dbc2126ae5db9166a526ed9517b43
dxie3 <davidxie04@gmail.com>
2024-01-07 12:39:52 -0500

    init

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Edit <code>src/App.js</code> and save to reload Ya Bitch.
         </p>
         <h1 className="text-3xl font-bold underline">
             Second Try!
