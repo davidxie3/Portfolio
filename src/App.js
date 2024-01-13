293fdd8d7a87a183c8a2d1bad8bfba454774b278
dxie3 <davidxie04@gmail.com>
2024-01-13 12:05:01 -0500

    added framer motion

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import logo from "./images/logo.svg";
 import "./App.css";
 import { Flex, VStack, Text } from "@chakra-ui/react";
+import { motion } from "framer-motion"
 
 function App() {
   return (
@@ -13,7 +14,15 @@ function App() {
     >
     
         <VStack align="center" spacing="4">
-          <img src={logo} className="App-logo" alt="logo" />
+        <motion.img
+          src={logo}
+
+          className="App-logo"
+          alt="logo"
+          animate={{ rotate: 360 }} // Rotate 360 degrees
+          transition={{ duration: 15, repeat: Infinity, ease: "linear" }}
+          style={{ width: "250px", height: "250px" }} // Rotate with a duration of 2 seconds, repeat infinitely, and use a linear easing function
+        />
           <Text fontFamily="FKDisplay Regular"> Site Under Construction!</Text>
         </VStack>
       </Flex>
